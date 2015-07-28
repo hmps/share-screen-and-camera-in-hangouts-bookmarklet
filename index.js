c8a61ab022bf42f525e8b636094f3abc1ed59b68
Ariel Scarpinelli <triforcexp@gmail.com>
2015-07-28 11:19:03 -0300

    Add missing colons. Fixed position at bottom.

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
 javascript:(function(){
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia
-  var mirror = document.querySelector('#lol-mirror')
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+  var mirror = document.querySelector('#lol-mirror');
 
   if (!mirror) {
     navigator.getUserMedia({video: true}, function(media) {
-      window.mirrorStream = media
+      window.mirrorStream = media;
 
         var div = document.createElement('video')
         , body = document.querySelector('body')
         , bodySteez = window.getComputedStyle(body)
         , bodyWidth = Number(bodySteez.width.replace(/px$/, ''))
         , bodyHeight = Number(bodySteez.height.replace(/px$/, ''))
-        , selfieWidth = bodyWidth / 3
+        , selfieWidth = bodyWidth / 6
         , selfieHeight = (selfieWidth * 3) / 4
-        , selfieTop = document.body.scrollTop
-        , selfieLeft = (bodyWidth /2) - (selfieWidth /2)
+        , selfieLeft = (bodyWidth /2) - (selfieWidth /2);
 
-      div.id = "lol-mirror"
-      div.style.width = selfieWidth + "px"
-      div.style.height = selfieHeight + "px"
-      div.style.position = "absolute"
-      div.style.top = selfieTop + "px"
-      div.style.left = selfieLeft + "px"
-      div.style.transform = "rotateY(180deg)"
-      div.style['-webkit-transform'] = "rotateY(180deg)" /* Safari and Chrome */
-      div.style['-moz-transform'] = 'rotateY(180deg)' /* Firefox */
-      div.style['z-index'] = 9999999
-      //div.style.backgroundColor = "red"
-      
-     
+      div.id = "lol-mirror";
+      div.style.width = selfieWidth + "px";
+      div.style.height = selfieHeight + "px";
+      div.style.position = "fixed";
+      div.style.bottom = 0;
+      div.style.left = selfieLeft + "px";
+      div.style.transform = "rotateY(180deg)";
+      div.style['z-index'] = 9999999;
 
-      document.querySelector('body').appendChild(div)
-      div.src = window.URL.createObjectURL(media)
-      div.play()
+      document.querySelector('body').appendChild(div);
+      div.src = window.URL.createObjectURL(media);
+      div.play();
      
       document.addEventListener('scroll', function(){
-        div.style.top = document.body.scrollTop + 'px'
+        div.style.top = document.body.scrollTop + 'px';
       })
       
     }, function(err){console.log(err)})
   } else {
-    mirror.parentElement.removeChild(mirror)
-    window.mirrorStream.stop()
+    mirror.parentElement.removeChild(mirror);
+    window.mirrorStream.stop();
   }
   
 })()
